398418d6504eb0d525a83a144ed9a1a3509c14e6
Javier Luraschi <javierluraschi@hotmail.com>
2019-02-22 00:43:33 -0800

    support for livy jars and fallback to 2.3 jar

--- R/dependencies.R
+++ R/dependencies.R
@@ -4,11 +4,18 @@ spark_dependencies <- function(spark_version, scala_version, ...) {
   sparklyr::spark_dependency(
     jars = c(
       system.file(
-        sprintf("java/sparkxgb-%s-%s.jar", spark_version, scala_version),
+        sprintf(
+          "java/sparkxgb-%s-%s.jar",
+          sparklyr::spark_dependency_fallback(spark_version, "2.3"),
+          scala_version
+        ),
         package = "sparkxgb"
       )
     ),
-    packages = "ml.dmlc:xgboost4j-spark:0.81"
+    packages = c(
+      "ml.dmlc:xgboost4j-spark:0.72"
+    ),
+    web_jars = "https://github.com/rstudio/sparkxgb/blob/master/inst/java/%s?raw=true"
   )
 }
 
