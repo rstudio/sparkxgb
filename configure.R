a8643db83c5cbc6485dd57fe90f0473dbb65eb9c
Javier Luraschi <javierluraschi@hotmail.com>
2019-02-22 00:45:27 -0800

    download jar dependencies if needed when compiling

--- configure.R
+++ configure.R
@@ -2,6 +2,13 @@
 
 library(purrr)
 
+if (!dir.exists("internal")) dir.create("internal")
+if (!dir.exists("internal/xgboost4j-spark")) dir.create("internal/xgboost4j-spark")
+if (!file.exists("internal/xgboost4j-spark/xgboost4j-spark-0.80.jar")) download.file(
+  "http://central.maven.org/maven2/ml/dmlc/xgboost4j-spark/0.80/xgboost4j-spark-0.80.jar",
+  "internal/xgboost4j-spark/xgboost4j-spark-0.80.jar"
+)
+
 spec <- sparklyr::spark_default_compilation_spec() %>%
   map(function(x) {
     x$jar_dep <- list.files("internal/xgboost4j-spark", full.names = TRUE) %>% 
