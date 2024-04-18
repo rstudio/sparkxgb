5cdf6c72f1c36ddb938711d3616dee58d1acca01
Edgar Ruiz <edgararuiz@gmail.com>
2024-04-18 23:38:43 +0000

    Re-enables tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -22,6 +22,6 @@ if (identical(Sys.getenv("NOT_CRAN"), "true")) {
   # timeout for downloading Apache Spark
   options(timeout = 300)
 
-  #test_check("sparkxgb")
+  test_check("sparkxgb")
   on.exit({spark_disconnect_all()})
 }
