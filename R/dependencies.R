a9d49f38581803b525e3d4372af91f91a0c9aaf0
Edgar Ruiz <edgararuiz@gmail.com>
2024-04-26 21:11:17 +0000

    Reverts to system.file for jar

--- R/dependencies.R
+++ R/dependencies.R
@@ -3,7 +3,7 @@ spark_dependencies <- function(spark_version, scala_version, ...) {
     stop(sprintf("Unsupported Scala version '%s'.", scala_version))
   }
   sparklyr::spark_dependency(
-    jars = package_file("java/sparkxgb-3.0-2.12.jar"),
+    jars = system.file("java/sparkxgb-3.0-2.12.jar", package = "sparkxgb"),
     packages = "ml.dmlc:xgboost4j-spark_2.12:2.0.3"
   )
 }
