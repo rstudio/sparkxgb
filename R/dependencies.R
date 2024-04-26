2ad7b09e22c88fc172a295a6366a5aabcff8cb5e
Edgar Ruiz <edgararuiz@gmail.com>
2024-04-26 21:04:40 +0000

    Reverts to using straight package name

--- R/dependencies.R
+++ R/dependencies.R
@@ -4,7 +4,7 @@ spark_dependencies <- function(spark_version, scala_version, ...) {
   }
   sparklyr::spark_dependency(
     jars = package_file("java/sparkxgb-3.0-2.12.jar"),
-    packages = readLines(package_file("maven/scala_212.txt"))
+    packages = "ml.dmlc:xgboost4j-spark_2.12:2.0.3"
   )
 }
 
